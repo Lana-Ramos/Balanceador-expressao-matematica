8584f578e80f2a7d3a86709d6bf4d6d09a3f99b6
Pedro Gabriel Ferraz Santos Silva <144126732+PGFerraz@users.noreply.github.com>
2025-03-23 12:27:51 -0300

    Parte de Pedro Gabriel - Função de verificação
    Criação da função de verificar o balanceamento da expressão, feito por Pedro Gabriel Ferraz Santos Silva

--- main.cpp
+++ main.cpp
@@ -3,22 +3,50 @@
 #include <stack>
 
 using namespace std;
- 
-bool verificar_balanceamento(string a); // protótipo da função
-// retorna True - expressão balanceada ou False - expressão não balanceada
+
+// True = Balanceada , False = Não Balanceada
+bool verificar_balanceamento(string expressao)
+{
+    // pilha para guardar elementos da expressão
+    stack<char> s;
+    
+    // Laço de repetição para iterar cada elemento da matriz 'c'
+    for (int i = 0; i < expressao.length(); ++i)
+    {
+        char c = expressao[i];
+
+        // Guardando os símbolos de agrupamento de abertura na pilha
+        if (c == '(' or c == '{' or c == '[')
+        {
+            s.push(c);
+        }
+        
+        // Verificação da igualdade entre os elementos da expressão e pilha
+        else if (c == ')' or c == '}' or c == ']') 
+        {
+            // Caso a pilha estaja vazia, irá retornar falso
+            if (s.empty()) return false; 
+
+            char top = s.top();
+            s.pop();
+
+            if (c == ')' && top != '(' or c == '}' && top != '{' or c == ']' && top != '[') 
+            {   
+                // Caso o elemento não seja correspondente, irá retornar falso
+                return false;
+            }
+        }
+    }
+
+    return s.empty();
+}
+
 int main()
 {
     string expressao, resp;
-    do{
-        cout<<"\nDigite a expressao que deseja verificar: ";
-        getline(cin, expressao);
-      
-        // fazer a mensagem que diz se está balanceada ou não
-      
-        verificar_balanceamento(expressao); // verifica se a expressão está balanceada
-        cout<<"\nDeseja verificar outra expressao?(sim/nao) ";
-        getline(cin, resp);
-        
+    do
+    {
+        // Interação com o usuário
     }while(resp=="sim" or resp =="Sim");
 
     return 0;
